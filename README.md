5a6a47deabb174f929ab1813029debe395770283
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-07-22 10:19:30 +0900

    feat: ConfigFile save/load

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Godot JSON Save/Load Test
 
-Godot 4.2.2
+- Godot 4.2.2
+- JSON Save/Load
+- ConfigFile Save/Load (.ini)
 
 see [GDScript](./node_2d.gd) for detail.
--- node_2d.gd
+++ node_2d.gd
@@ -5,6 +5,10 @@ var saved_message = "Hello, World!"
 var last_saved_date = null
 
 func _ready():
+	load_json()
+	load_ini()
+
+func load_json():
 	# if json exists, load save data
 	if ResourceLoader.exists("user://save.json"):
 		var f = FileAccess.open("user://save.json", FileAccess.READ)
@@ -18,15 +22,46 @@ func _ready():
 		f.close()
 
 		# log the saved message
-		print("data loaded")
+		print("[JSON] data loaded")
 		print("saved message: ", saved_message)
 		print("last saved date: ", last_saved_date)
 	else:
-		print("no save data found")
+		print("[JSON] no save data found")
 
 
 	# save the message
-	save_message()
+	save_json()
+
+func load_ini():
+	var config = ConfigFile.new()
+	var err = config.load("user://save.ini")
+
+	if err == OK:
+		saved_message = config.get_value("save", "message", "Hello, World!")
+		last_saved_date = config.get_value("save", "date", null)
+
+		# log the saved message
+		print("[INI] data loaded")
+		print("saved message: ", saved_message)
+		print("last saved date: ", last_saved_date)
+	else:
+		print("[INI] no save data found")
+
+	# save the message
+	save_ini()
+
+func save_ini():
+	# save the message
+	var config = ConfigFile.new()
+	var date = Time.get_date_dict_from_system()
+	var time = Time.get_time_dict_from_system()
+	var date_str = str(date.year) + "-" + zp(date.month) + "-" + zp(date.day) + " " + zp(time.hour) + ":" + zp(time.minute) + ":" + zp(time.second)
+
+	config.set_value("save", "message", "Saved at " + date_str)
+	config.set_value("save", "date", date_str)
+	config.save("user://save.ini")
+
+	print("[INI] data saved")
 
 ## zero pad a number, like 1 -> 01
 func zp(num):
@@ -35,7 +70,7 @@ func zp(num):
 	else:
 		return str(num)
 
-func save_message():
+func save_json():
 	# save the message
 	var dict = {}
 	var date = Time.get_date_dict_from_system()
@@ -48,7 +83,7 @@ func save_message():
 	f.store_string(JSON.stringify(dict))
 	f.close()
 
-	print("data saved")
+	print("[JSON] data saved")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
